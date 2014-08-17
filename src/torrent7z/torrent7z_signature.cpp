14ac0988612e69190a0f6b1a83279572dda27be2
madmonkey1907 <madmonkey1907@5c365660-8b63-4ee5-8767-1b43b189e8d4>
2014-08-17 19:17:40 +0000

    use external uncompressor when possible

--- src/torrent7z/torrent7z_signature.cpp
+++ src/torrent7z/torrent7z_signature.cpp
@@ -13,7 +13,7 @@ namespace torrent7z
 //                 16-byte seed                                                        signature
 const char*t7zsig="\xa9\x9f\xd1\x57\x08\xa9\xd7\xea\x29\x64\xb2\x36\x1b\x83\x52\x33\x00torrent7z_0.9beta";
 //const int t7zsig_size=16+1+9+4+4;
-const char*t7zsig_str="torrent7z_0.9.1beta"
+const char*t7zsig_str="torrent7z_0.9.2beta"
 #ifdef DEBUG
 "(debug)"
 #else
